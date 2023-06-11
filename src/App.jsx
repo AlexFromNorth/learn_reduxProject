498b9c9c3e74b5e471e808672001827a7b65e1d2
AlexFromNorth <jacklex.ab@gmail.com>
2023-06-11 16:17:03 +0300

    created pet-project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import RecipeItem from './recipe-item/RecipeItem'
+
+function App() {
+
+  return (
+    <div>
+      <RecipeItem recipe={{
+        id: 1,
+        name: 'Лазанья',
+      }}/>
+      <RecipeItem recipe={{
+        id: 2,
+        name: 'Пирог',
+      }}/>
+      <RecipeItem recipe={{
+        id: 3,
+        name: 'Борщ',
+      }}/>
+    </div>
+  )
+}
+
+export default App
